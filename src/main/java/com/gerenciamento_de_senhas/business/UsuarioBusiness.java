08e66a2c1bc1ed967df3f01a4a866906902fced9
George Neves <50254856+geneves97@users.noreply.github.com>
2023-08-30 15:47:41 -0300

    Feat3 (#5)
    * feat[#1]: Criando endpoint, um metodo para descriptografar as senhas e exibindo em uma lista.
    
    * feat[#1]: Criando endpoint, um metodo para deletar um usuario e todas as senhas ligadas a ele.

--- src/main/java/com/gerenciamento_de_senhas/business/UsuarioBusiness.java
+++ src/main/java/com/gerenciamento_de_senhas/business/UsuarioBusiness.java
@@ -2,6 +2,8 @@ package com.gerenciamento_de_senhas.business;
 
 import java.util.List;
 
+import jakarta.transaction.Transactional;
+import org.antlr.v4.runtime.TokenStreamRewriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,10 +17,14 @@ public class UsuarioBusiness {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
-    
-    private static final int MAX_PASSWORDS_PER_USER = 3;
-    
-    public UsuarioEntity save(UsuarioEntity usuarioEntity) throws Exception {
+
+	@Autowired
+	private GerenciamentoRepository gerenciamentoRepository; // Injetar o GerenciamentoRepository corretamente
+
+
+	private static final int MAX_PASSWORDS_PER_USER = 3;
+
+	public UsuarioEntity save(UsuarioEntity usuarioEntity) throws Exception {
 		usuarioEntity.setNome(usuarioEntity.getNome());
 		return usuarioRepository.save(usuarioEntity);
 	}
@@ -38,4 +44,15 @@ public class UsuarioBusiness {
 		    UsuarioEntity usuario = findById(userId);
 		    return usuario.getSenhas().size() >= MAX_PASSWORDS_PER_USER;
 		}
+	@Transactional
+	public void deleteUsuarioById(Integer id) {
+		UsuarioEntity usuario = findById(id);
+		List<GerenciamentoEntity> senhas = usuario.getSenhas();
+
+		for (GerenciamentoEntity senha : senhas) {
+			gerenciamentoRepository.delete(senha);
+		}
+
+		usuarioRepository.delete(usuario);
+	}
 }
--- src/main/java/com/gerenciamento_de_senhas/controller/UsuarioController.java
+++ src/main/java/com/gerenciamento_de_senhas/controller/UsuarioController.java
@@ -3,11 +3,14 @@ package com.gerenciamento_de_senhas.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import com.gerenciamento_de_senhas.business.UsuarioBusiness;
 import com.gerenciamento_de_senhas.entity.GerenciamentoEntity;
@@ -38,4 +41,14 @@ public class UsuarioController {
 	    UsuarioEntity usuario = usuarioBusiness.findById(id);
 	    return usuario.getSenhas();
 	    }
+
+	@DeleteMapping("/usuarios/{id}")
+	public ResponseEntity<String> deleteUsuarioById(@PathVariable Integer id) {
+		try {
+			usuarioBusiness.deleteUsuarioById(id);
+			return ResponseEntity.ok("Usuário e suas senhas foram excluídos com sucesso.");
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao excluir usuário e suas senhas.");
+		}
+	}
 }
